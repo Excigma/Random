1c9a2e3eeddb0b1d49d25a36961ec6fdec48eb2b
Excigma <30280397+Excigma@users.noreply.github.com>
2024-03-08 23:54:39 +1300

    feat: its working but as a touchscreen lol??

--- web-uinput-touchpad/Go/main.go
+++ web-uinput-touchpad/Go/main.go
@@ -18,6 +18,7 @@ var upgrader = websocket.Upgrader{
 func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		var touchpad uinput.MultiTouch
+
 		conn, wsErr := upgrader.Upgrade(w, r, nil)
 		if wsErr != nil {
 			return
@@ -26,8 +27,10 @@ func main() {
 		for {
 			/**
 			 * Protocol (angle brackets are to be omitted):
-			 * - 0 <width> <height> 0\n: Initialize a new device with dimensions width and height
-			 * - 1 <type> <code> <value>\n: Call send_uinput_event with type, code, and value
+			 * - 0 <width> <height>: Initialize a new device with dimensions width and height
+			 * - 1 <slot> <x> <y>: Simulate a new touch contact at slot with coordinates x and y
+			 * - 2 <slot> <x> <y>: Simulate a touch contact moving to coordinates x and y
+			 * - 3 <slot>: Simulate a touch contact being lifted
 			 */
 			_, msg, err := conn.ReadMessage()
 			if err != nil {
@@ -36,7 +39,7 @@ func main() {
 
 			// Parse command, one two and three from the message
 			// Split the string into individual numbers
-			msgSplit := strings.Split(string(msg), " ")
+			msgSplit := strings.Split(strings.Trim(string(msg), "\n"), " ")
 
 			// Initialize a slice to store parsed integers
 			var numbers []int32
@@ -51,7 +54,18 @@ func main() {
 				numbers = append(numbers, int32(num))
 			}
 
+			// Print the parsed numbers
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
 			if numbers[0] == 0 {
+				// Init packet to initialize a new touchpad device
+				// width, height
+				// Close the previous touchpad if it exists
+				if touchpad != nil {
+					touchpad.Close()
+				}
+
+				// Create a new touchpad with the given dimensions
 				touchpad, err = uinput.CreateMultiTouch(
 					"/dev/uinput",
 					[]byte("virtual-touchpad"),
@@ -64,11 +78,16 @@ func main() {
 					return
 				}
 				defer touchpad.Close()
-
-				// touchpad.GetContacts()[0].id
-				fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-			} else if numbers[0] == 1 {
-
+			} else if numbers[0] == 1 || numbers[0] == 2 {
+				// Touchdown packet to simulate a new touch contact
+				// slot, x, y
+				// Simulate a new touch contact
+				touchpad.GetContacts()[numbers[1]].TouchDownAt(numbers[2], numbers[3])
+			} else if numbers[0] == 3 {
+				// Touchup packet to simulate a touch contact being lifted
+				// slot
+				// Simulate a touch contact being lifted
+				touchpad.GetContacts()[numbers[1]].TouchUp()
 			}
 		}
 	})
